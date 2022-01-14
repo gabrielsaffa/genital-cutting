0f550932023ffa0cad87430b067ba00d03e331ca
gabrielsaffa <gabriel.saffa@azet.sk>
2022-01-14 21:50:36 +0100

    Update GM_script-phylogenetic analyses.R

--- GM_script-phylogenetic analyses.R
+++ GM_script-phylogenetic analyses.R
@@ -1,6 +1,6 @@
-#########################################################################################################
-### R script for: Global phylogenetic analysis reveals multiple origins of genital mutilation/cutting ###
-#########################################################################################################
+###############################################################################################################################
+### R script  for: Global phylogenetic analysis reveals multiple origins and socio-ecological correlates of genital cutting ###
+###############################################################################################################################
 
 ### phylogenetic signal, ancestral state reconstructions and correlated evolution ###
 
