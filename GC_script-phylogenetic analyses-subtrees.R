cab2e4ef77cdad43a52c13646f2b074ef2c7c914
Gabriel Šaffa <gabriel.saffa@azet.sk>
2022-02-01 11:21:23 +0100

    Update GC_script-phylogenetic analyses-subtrees.R

--- GC_script-phylogenetic analyses-subtrees.R
+++ GC_script-phylogenetic analyses-subtrees.R
@@ -1,5 +1,5 @@
 ###############################################################################################################################
-### R script  for: Global phylogenetic analysis reveals multiple origins and socio-ecological correlates of genital cutting ###
+### R script  for: Global phylogenetic analysis reveals multiple origins and correlates of genital cutting ###
 ###############################################################################################################################
 
 ### ancestral state reconstructions of GM for individual language families ###
